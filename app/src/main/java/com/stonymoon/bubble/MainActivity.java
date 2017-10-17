aebab11c59be6c57efc850bbee4b16c1ad96e8ca
StonyMoon <2365975967@qq.com>
2017-10-17 18:50:57 +0800

    添加高德定位

--- app/src/main/java/com/stonymoon/bubble/MainActivity.java
+++ app/src/main/java/com/stonymoon/bubble/MainActivity.java
@@ -1,5 +1,9 @@
 package com.stonymoon.bubble;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,6 +18,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.vondear.rxtools.RxPermissionsTool;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -26,6 +40,8 @@ public class MainActivity extends AppCompatActivity
     Toolbar toolbar;
     @BindView(R.id.fab)
     FloatingActionButton fab;
+    private AMapLocationClient locationClient = null;
+    private AMapLocationClientOption locationOption = null;
 
     @OnClick(R.id.fab)
     void submit() {
@@ -40,6 +56,13 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        RxPermissionsTool.with(this).addPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                .addPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .addPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .addPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .addPermission(Manifest.permission.READ_PHONE_STATE)
+                .initPermission();
+
         textView.setText("hello bubble");
         setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -50,6 +73,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        initLocation();
+        startLocation();
     }
 
     @Override
@@ -108,4 +133,92 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private void initLocation() {
+        /**
+         * 定位监听
+         */
+        //监听器
+        AMapLocationListener locationListener = new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation location) {
+                if (null != location) {
+
+                    StringBuffer sb = new StringBuffer();
+                    //errCode等于0代表定位成功，其他的为定位失败，具体的可以参照官网定位错误码说明
+                    if (location.getErrorCode() == 0) {
+                        sb.append("定位成功" + "\n");
+                        sb.append("定位类型: " + location.getLocationType() + "\n");
+                        sb.append("经    度    : " + location.getLongitude() + "\n");
+                        sb.append("纬    度    : " + location.getLatitude() + "\n");
+                        sb.append("精    度    : " + location.getAccuracy() + "米" + "\n");
+                        sb.append("提供者    : " + location.getProvider() + "\n");
+                        sb.append("速    度    : " + location.getSpeed() + "米/秒" + "\n");
+                        sb.append("角    度    : " + location.getBearing() + "\n");
+                        // 获取当前提供定位服务的卫星个数
+                        sb.append("星    数    : " + location.getSatellites() + "\n");
+                        sb.append("国    家    : " + location.getCountry() + "\n");
+                        sb.append("省            : " + location.getProvince() + "\n");
+                        sb.append("市            : " + location.getCity() + "\n");
+                        sb.append("城市编码 : " + location.getCityCode() + "\n");
+                        sb.append("区            : " + location.getDistrict() + "\n");
+                        sb.append("区域 码   : " + location.getAdCode() + "\n");
+                        sb.append("地    址    : " + location.getAddress() + "\n");
+                        sb.append("兴趣点    : " + location.getPoiName() + "\n");
+                        //定位完成的时间
+                    } else {
+                        //定位失败
+                        sb.append("定位失败" + "\n");
+                        sb.append("错误码:" + location.getErrorCode() + "\n");
+                        sb.append("错误信息:" + location.getErrorInfo() + "\n");
+                        sb.append("错误描述:" + location.getLocationDetail() + "\n");
+                    }
+                    sb.append("***定位质量报告***").append("\n");
+                    sb.append("* WIFI开关：").append(location.getLocationQualityReport().isWifiAble() ? "开启" : "关闭").append("\n");
+                    //解析定位结果，
+                    String result = sb.toString();
+                    textView.setText(result);
+                } else {
+                    textView.setText("定位失败，loc is null");
+                }
+            }
+        };
+
+
+        //初始化client
+        locationClient = new AMapLocationClient(this.getApplicationContext());
+        locationOption = getDefaultOption();
+        //设置定位参数
+        locationClient.setLocationOption(locationOption);
+        // 设置定位监听
+        locationClient.setLocationListener(locationListener);
+    }
+
+    private AMapLocationClientOption getDefaultOption() {
+        AMapLocationClientOption mOption = new AMapLocationClientOption();
+        mOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
+        mOption.setGpsFirst(false);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
+        mOption.setHttpTimeOut(30000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
+        mOption.setInterval(2000);//可选，设置定位间隔。默认为2秒
+        mOption.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
+        mOption.setOnceLocation(false);//可选，设置是否单次定位。默认是false
+        mOption.setOnceLocationLatest(false);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
+        AMapLocationClientOption.setLocationProtocol(AMapLocationClientOption.AMapLocationProtocol.HTTP);//可选， 设置网络请求的协议。可选HTTP或者HTTPS。默认为HTTP
+        mOption.setSensorEnable(false);//可选，设置是否使用传感器。默认是false
+        mOption.setWifiScan(true); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
+        mOption.setLocationCacheEnable(true); //可选，设置是否使用缓存定位，默认为true
+        return mOption;
+    }
+
+
+    private void startLocation() {
+        // 设置定位参数
+        locationClient.setLocationOption(locationOption);
+        // 启动定位
+        locationClient.startLocation();
+    }
+
+
+
+
 }
