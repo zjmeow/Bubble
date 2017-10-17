66a9680b3e0c52b9fce5e021bba92712909da4e0
StonyMoon <2365975967@qq.com>
2017-10-17 22:46:01 +0800

    添加气泡和触摸反馈

--- app/src/main/java/com/stonymoon/bubble/MapActivity.java
+++ app/src/main/java/com/stonymoon/bubble/MapActivity.java
@@ -1,11 +1,19 @@
 package com.stonymoon.bubble;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
+import com.qmuiteam.qmui.widget.dialog.QMUIBottomSheet;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,20 +25,34 @@ public class MapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        QMUIStatusBarHelper.translucent(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         ButterKnife.bind(this);
         mapView.onCreate(savedInstanceState);
         AMap aMap = mapView.getMap();
 
-        MyLocationStyle myLocationStyle;
-        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        MyLocationStyle myLocationStyle = new MyLocationStyle();//初始化
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER);
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+        //aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，并且蓝点会跟随设备移动。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
 
+        LatLng latLng = new LatLng(39.906901, 116.397972);
+        final Marker marker = aMap.addMarker(new MarkerOptions().position(latLng).title("北京").snippet("Infowindow"));
+        // 定义 Marker 点击事件监听
+        AMap.OnMarkerClickListener markerClickListener = new AMap.OnMarkerClickListener() {
+            // marker 对象被点击时回调的接口
+            // 返回 true 则表示接口已响应事件，否则返回false
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                showSimpleBottomSheetGrid();
+                return true;
+            }
+        };
+        // 绑定 Marker 被点击事件
+        aMap.setOnMarkerClickListener(markerClickListener);
 
     }
 
@@ -61,6 +83,49 @@ public class MapActivity extends AppCompatActivity {
         //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
         mapView.onSaveInstanceState(outState);
     }
+
+
+    private void showSimpleBottomSheetGrid() {
+        final Context context = MapActivity.this;
+        final int TAG_SHARE_WECHAT_FRIEND = 0;
+        final int TAG_SHARE_WECHAT_MOMENT = 1;
+        final int TAG_SHARE_WEIBO = 2;
+        final int TAG_SHARE_CHAT = 3;
+        final int TAG_SHARE_LOCAL = 4;
+        QMUIBottomSheet.BottomGridSheetBuilder builder = new QMUIBottomSheet.BottomGridSheetBuilder(context);
+        builder.addItem(R.mipmap.test, "分享到微信", TAG_SHARE_WECHAT_FRIEND, QMUIBottomSheet.BottomGridSheetBuilder.FIRST_LINE)
+                .addItem(R.mipmap.test, "分享到朋友圈", TAG_SHARE_WECHAT_MOMENT, QMUIBottomSheet.BottomGridSheetBuilder.FIRST_LINE)
+                .addItem(R.mipmap.test, "分享到微博", TAG_SHARE_WEIBO, QMUIBottomSheet.BottomGridSheetBuilder.FIRST_LINE)
+                .addItem(R.mipmap.test, "分享到私信", TAG_SHARE_CHAT, QMUIBottomSheet.BottomGridSheetBuilder.FIRST_LINE)
+                .addItem(R.mipmap.test, "保存到本地", TAG_SHARE_LOCAL, QMUIBottomSheet.BottomGridSheetBuilder.SECOND_LINE)
+                .setOnSheetItemClickListener(new QMUIBottomSheet.BottomGridSheetBuilder.OnSheetItemClickListener() {
+                    @Override
+                    public void onClick(QMUIBottomSheet dialog, View itemView) {
+                        dialog.dismiss();
+                        int tag = (int) itemView.getTag();
+                        switch (tag) {
+                            case TAG_SHARE_WECHAT_FRIEND:
+                                Toast.makeText(context, "分享到微信", Toast.LENGTH_SHORT).show();
+                                break;
+                            case TAG_SHARE_WECHAT_MOMENT:
+                                Toast.makeText(context, "分享到朋友圈", Toast.LENGTH_SHORT).show();
+                                break;
+                            case TAG_SHARE_WEIBO:
+                                Toast.makeText(context, "分享到微博", Toast.LENGTH_SHORT).show();
+                                break;
+                            case TAG_SHARE_CHAT:
+                                Toast.makeText(context, "分享到私信", Toast.LENGTH_SHORT).show();
+                                break;
+                            case TAG_SHARE_LOCAL:
+                                Toast.makeText(context, "保存到本地", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                    }
+                }).build().show();
+
+
+    }
+
 }
 
 
